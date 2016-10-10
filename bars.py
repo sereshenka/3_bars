a14203b401679c34496b220646e2be140f8abd91
sereshenka <sergey.kolesnikov30@yandex.ru>
2016-10-10 18:45:52 +0400

    Changed name of some variables,support incorrect input

--- bars.py
+++ bars.py
@@ -10,38 +10,48 @@ urllib.request.urlretrieve(url, destination)
 z = zipfile.ZipFile('bari.zip', 'r')
 z.extractall()
 z.close()
-print('Введите,пожалуйста,ваши координаты(вначале долготу,потом широту')
-x1 = float(input())
-y1 = float(input())
+print('Введите,пожалуйста,ваши координаты')
+try:
+    longitude = float(input('Долгота:'))
+except ValueError:
+    longitude = None
+if longitude is None:
+    print('Неверный формат')
+try:
+    latitude = float(input('Широта:'))
+except ValueError:
+    latitude = None
+if latitude is None:
+    print('Неверный формат')
 with open('Бары.json',encoding='utf-8') as json_data:
     data=json.load(json_data)
     places = [dat["Cells"]["SeatsCount"] for dat in data]
     
 def biggest_bar(data,places):
     max1 = max(places)
-    big_bar = [dat["Cells"]["Name"] for dat in data if max1 == dat["Cells"]["SeatsCount"]]
-    print (big_bar)
+    big_bar = [dat["Cells"]["Name"] for dat in data if max1 is dat["Cells"]["SeatsCount"]]
+    print ('Самый большой бар:',big_bar)
     
 def smallest_bar(data,places):
     min1 = min(places)
-    small_bar = [dat["Cells"]["Name"] for dat in data if min1 == dat["Cells"]["SeatsCount"]]
-    print(small_bar[1])
+    small_bar = [dat["Cells"]["Name"] for dat in data if min1 is dat["Cells"]["SeatsCount"]]
+    print('Самый маленький бар:',small_bar[1])
     
-def geo_bar(data,x1,y1):
+def geo_bar(data,longitude,latitude):
     distance=[]
     for dat in data:
-        x2 = dat["Cells"]["geoData"]["coordinates"][0]
-        y2 = dat["Cells"]["geoData"]["coordinates"][1]
-        distance.append(hypot(x2 - x1, y2 - y1))
+        longitude1 = dat["Cells"]["geoData"]["coordinates"][0]
+        latitude1 = dat["Cells"]["geoData"]["coordinates"][1]
+        distance.append(hypot(longitude1 - longitude, latitude1 - latitude))
     min2 = min(distance)
     for dat in data:
-        x2 = dat["Cells"]["geoData"]["coordinates"][0]
-        y2 = dat["Cells"]["geoData"]["coordinates"][1]
-        rast = hypot(x2 - x1, y2 - y1)
+        longitude1 = dat["Cells"]["geoData"]["coordinates"][0]
+        latitude1 = dat["Cells"]["geoData"]["coordinates"][1]
+        rast = hypot(longitude1 - longitude, latitude1 - latitude)
         if min2 == rast :
             nearest_bar = dat["Cells"]["Name"] 
-    print(nearest_bar)
+    print('Самый близкий бар относительно вашей геопозиции:',nearest_bar)
     
 biggest_bar(data,places)
 smallest_bar(data,places)
-geo_bar(data,x1,y1)
+geo_bar(data,longitude,latitude)
