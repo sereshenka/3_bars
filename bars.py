01440e983a0a02dabb7e2cae8bb2bdb6d7cb49c2
sereshenka <sergey.kolesnikov30@yandex.ru>
2016-11-01 19:08:13 +0300

    добавил tempfile,исправил праивло с stdout

--- bars.py
+++ bars.py
@@ -3,15 +3,15 @@ import zipfile
 import json
 from math import hypot
 import os
+import tempfile
 
 def input1():
     try:
         longitude = float(input('Долгота:'))
-        return (longitude)
     except ValueError:
         longitude = None
-    if longitude is None:
-        print('Неверный формат')
+    return longitude
+        
 
 def input2():
     try:
@@ -19,22 +19,23 @@ def input2():
         return (latitude)
     except ValueError:
         latitude = None
-    if latitude is None:
-        print('Неверный формат')
+    return latitude
 
-def get_zip():
-    destination = 'bari.zip'
+
+def get_zip(tmpdirectory):
+    destination = os.path.join(tmpdirectory,'bari.zip')
     url = 'http://data.mos.ru/opendata/export/1796/json/2/1'
     urllib.request.urlretrieve(url, destination)
-    return (destination)
+    if not os.path.exists(destination):
+        return None
+    return destination
+
 
 def extract_zip(destination):
-    if not os.path.exists(destination):
-        print('Zip архив не скачался')
-    else:
-        z = zipfile.ZipFile(destination, 'r')
-        z.extractall()
-        z.close()
+    z = zipfile.ZipFile(destination, 'r')
+    z.extractall()
+    z.close()
+
         
 def open_json():
     try:
@@ -42,41 +43,51 @@ def open_json():
             return (json.load(json_data))
     except OSError :
         dest = None
-    if dest is None:
-        print('Список баров отсутствует')
-        
+        return dest
+          
     
 def biggest_bar(data):
-    return(max(data, key = lambda x: x["Cells"]["SeatsCount"]))
+    return max(data, key = lambda x: x["Cells"]["SeatsCount"])
+
     
 def smallest_bar(data):
-    return (min(data, key = lambda x: x["Cells"]["SeatsCount"]))
-    
+    return min(data, key = lambda x: x["Cells"]["SeatsCount"])
+
 
 def geo_bar(data,longitude,latitude):
     bar_key = lambda x: hypot(x["Cells"]["geoData"]["coordinates"][0]-longitude,x["Cells"]["geoData"]["coordinates"][1]-latitude)
     geo = min(data, key = bar_key )
-    return (geo)
+    return geo
+
     
 def print_all(big_bar,small_bar,geo):
-    try:
-        print('Самый большой бар:',big_bar["Cells"]["Name"])
-        print ('Самый маленький бар:',small_bar["Cells"]["Name"])
-        print('Ближайший бар относительно вашей геопозиции:', geo["Cells"]["Name"])
-    except TypeError :
-        pass
+    print('Самый большой бар:',big_bar["Cells"]["Name"])
+    print ('Самый маленький бар:',small_bar["Cells"]["Name"])
+    print('Ближайший бар относительно вашей геопозиции:', geo["Cells"]["Name"])
+    
+
     
 if __name__ == '__main__':
-    print('Введите,пожалуйста,ваши координаты')
-    longitude = input1()
-    latitude = input2()
-    if longitude and latitude is not None:
-        destination = get_zip()
-        extract_zip(destination)
-        data = open_json()
-        if data is not None:
-            big_bar = biggest_bar(data)
-            small_bar = smallest_bar(data)
-            geo = geo_bar(data,longitude,latitude)
-            print_all(big_bar,small_bar,geo)
-
+    while True:
+        with tempfile.TemporaryDirectory() as tmpdirectory:
+            print(tmpdirectory)
+            destination = get_zip(tmpdirectory)
+            if destination is None:
+                print('Zip архив не скачался')
+                break
+            zip_archive = extract_zip(destination)
+            data = open_json()
+            if data is None:
+                print('Список баров отсутствует')
+                break
+        print('Введите,пожалуйста,ваши координаты')
+        longitude = input1()
+        latitude = input2()
+        if longitude is None or latitude is None:
+            print('Неверный формат')
+            break
+        big_bar = biggest_bar(data)
+        small_bar = smallest_bar(data)
+        geo = geo_bar(data,longitude,latitude)
+        print_all(big_bar,small_bar,geo)
+        break
